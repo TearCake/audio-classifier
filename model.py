8a52ad6fc7cd7ac3c1608f61c598e8132ed4c182
TearCake <adityachavan1357@gmail.com>
2026-06-04 09:49:17 +0530

    first commit

---
+++ model.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, 3, 
+                               stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, 3, 
+                               stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.shortcut = nn.Sequential()
+        self.use_shortcut = stride != 1 or in_channels != out_channels
+        if self.use_shortcut:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, 1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = torch.ReLU(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        shortcut = self.shortcut(x) if self.use_shortcut else x
+        out_add = out + shortcut
+        out = torch.ReLU(out_add)
+        return out
+    
+class AudioCNN(nn.Module):
+    def __init__(self, num_classes=50):
+        super().__init__()
+        self.conv1 = nn.Sequential(nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False), nn.BatchNorm2d(64), nn.ReLU(inplace=True))
